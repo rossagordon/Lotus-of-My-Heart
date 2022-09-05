bcf8ec3e350e4e4fe14c9c2363bcf2f01232fd4b
Ross Gordon <rossagordonstl@gmail.com>
2022-09-04 22:28:36 -0400

    Alignment issue fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,23 +11,23 @@ const About = () => {
       <div className="justify-center items-center mx-auto p-8">
         <p className="text-5xl font-bold pb-8 ">I'm Jelena</p>
         <div>
-          <img src={Jelena} alt="Jelena" />
+          <img src={Jelena} className="" alt="Jelena" />
         </div>
       </div>
-      <div className="p-4 pt-[75px]">
-        <p className="text-3xl text-[#716040]">
+      <div className="p-4 sm:pt-[115px] ">
+        <p className="text-2xl text-[#716040]">
           I tattoo out of a private studio in Richmond, VA. I began my tattoo
           journey in Long Island, NY. Over a 10 year period, I tattooed in NYC,
           Chicago, and St.Louis. During this time, I also traveled throughout
           the US, tattooing at conventions and on guest spots.
         </p>
-        <p className="text-3xl text-[#716040] pt-4">
+        <p className="text-2xl text-[#716040] pt-4">
           I slowed down in my tattoo related travels a few years back, but still
           very much enjoy traveling recreationally. My favorite places I've
           traveled to are Costa Rica, Guatemala and Thailand. I can talk about
           traveling all day long!
         </p>
-        <p className="text-3xl text-[#716040] pt-4">
+        <p className="text-2xl text-[#716040] pt-4">
         Aside from tattooing, my interests include Yoga, studying yoga philosophy, reading, and long walks with my dog.
         </p>
       </div>
